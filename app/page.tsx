ae648971cc699a6976f571ac1977bfc4a18cb6db
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2024-09-04 20:09:41 +0545

    How it works section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
 import { Button } from "@/components/ui/button";
 import Banner from "@/components/home/banner";
 import BgGradient from "@/components/common/bg-gradient";
+import { Dot } from "lucide-react";
+import HowItWorks from "@/components/home/howitworks";
+import Pricing from "@/components/home/pricing";
 
 export default function Home() {
   return (
-    <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px), transparent_1px] [background-size:16px_16px]">
+    <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
       <BgGradient />
       <Banner />
-      {/* <Divider />
+      <div className="flex items-center justify-center">
+        <Dot className="text-purple-400"></Dot>
+        <Dot className="text-purple-400"></Dot>
+        <Dot className="text-purple-400"></Dot>
+      </div>
       <HowItWorks />
-      <Divider />
+      <div className="flex items-center justify-center">
+        <Dot className="text-purple-400"></Dot>
+        <Dot className="text-purple-400"></Dot>
+        <Dot className="text-purple-400"></Dot>
+      </div>
       <Pricing />
-      <Divider />
-      <Footer /> */}
+      <div className="flex items-center justify-center">
+        <Dot className="text-purple-400"></Dot>
+        <Dot className="text-purple-400"></Dot>
+        <Dot className="text-purple-400"></Dot>
+      </div>
+      {/* <Footer /> */}
     </main>
   );
 }
