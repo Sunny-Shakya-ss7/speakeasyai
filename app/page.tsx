001ac04949413c3ff7cbf6c98225ec847fdfea4d
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2024-09-04 20:23:39 +0545

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,12 @@ export default function Home() {
         <Dot className="text-purple-400"></Dot>
         <Dot className="text-purple-400"></Dot>
       </div>
-      {/* <Footer /> */}
+      <footer className="bg-gray-200/20 flex h-20 py-24 px-12 z-20 relative overflow-hidden flex-col gap-2">
+        <p>All Rights Reserved, {new Date().getFullYear()}</p>
+        <a href="https://x.com/@kulkarniankita9" target="_blank">
+          Built by Ankita 🚀
+        </a>
+      </footer>
     </main>
   );
 }
